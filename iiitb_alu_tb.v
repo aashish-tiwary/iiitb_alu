9b657252711fa30ca7419310dfb6947afee8afd0
aashish-tiwary <110485513+aashish-tiwary@users.noreply.github.com>
2022-09-09 23:28:19 +0530

    Update iiitb_alu_tb.v

--- iiitb_alu_tb.v
+++ iiitb_alu_tb.v
@@ -1,33 +1,48 @@
-odule tb_alu;
+module iiitb_alu_tb;
 
-    // Inputs
-    reg [7:0] A;
-    reg [7:0] B;
-    reg [2:0] Op;
+// Inputs
+reg [7:0] A;
+reg [7:0] B;
+reg [2:0] op;
+reg clk;
 
-    // Outputs
-    wire [7:0] R;
+// Outputs
+wire [7:0] R;
 
-    // Instantiate the Unit Under Test (UUT)
-    ALU uut ( 
-        .A(A), 
-        .B(B), 
-        .Op(Op), 
-        .R(R)
-    );
+// Instantiate the Unit Under Test (UUT)
+iiitb_alu uut ( 
+.clk(clk),
+.A(A), 
+.B(B), 
+.op(op), 
+.R(R)
+);
     
-    initial begin
-        // Apply inputs.
-        A = 8'b01101010;
-        B = 8'b00111011;
-        Op = 0; #100;
-        Op = 1; #100;
-        Op = 2; #100;
-        Op = 3; #100;
-        Op = 4; #100;
-        Op = 5; #100;
-        Op = 6; #100;
-        Op = 7; #100;
-    end
-      
+initial begin
+
+$dumpfile ("iiitb_alu_out.vcd"); 
+$dumpvars(0,iiitb_alu_tb);
+
+// Initialize Inputs
+
+clk = 0;
+
+end
+always 
+#100 clk=~clk;
+    
+initial begin
+// Apply inputs.
+A = 8'b01101010;
+B = 8'b00111011;
+op = 0; #100;
+op = 1; #100;
+op = 2; #100;
+op = 3; #100;
+op = 4; #100;
+op = 5; #100;
+op = 6; #100;
+op = 7; #100;
+end
+
 endmodule
